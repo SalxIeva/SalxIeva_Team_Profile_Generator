22edf04c7b929e4d56a6780acb09e3fe2efee0ba
SalxIeva <ieva.saldukaite@gmail.com>
2023-12-27 22:29:18 +0000

    pseudo code update for all js

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-// create a function to start generator
-// initialize generator
-// create an array to store team members
+
+// create a manager function
+// prompt for managers details
+// add the manager to the team array
+
+// create a engineer function
+// prompt for engineer details
+// add the engineer to the team array
+
+// create an intern function
+// prompt for intern details
+// add intern to the team array 
+
+// create a function to start generator by first prompting user for managers details
+//  add team members to the array
