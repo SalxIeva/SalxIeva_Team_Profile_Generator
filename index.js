f0c0f7cb4b2551f8c880a219eb61253336cca6b0
SalxIeva <ieva.saldukaite@gmail.com>
2023-12-29 18:47:19 +0000

    employee class added to employee.js

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+// create an array to store team members
+//  add team members to the array
+const teamMembers = [];
+
+// create a function to start generator by first prompting user for managers details
+function startGenerator() {
+    createManager();
+}
 
 // create a manager function
 function createManager() {
@@ -38,11 +46,15 @@ function createManager() {
             name: "officeNumber",
             message: "What is the manager's office number?"
         }
-    ])
-    // add the manager to the team array
+        // add the manager to the team array
+    ]).then(answers => {
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+        teamMembers.push(manager);
+    });
 
 }
-createManager();
+
+startGenerator();
 // create a engineer function
 // function createEngineer() {
 //     // prompt for engineer details
@@ -56,9 +68,3 @@ createManager();
 //     // add intern to the team array 
 
 // }
-
-// create a function to start generator by first prompting user for managers details
-// function startGenerator() {
-//     createManager();
-// }
-//  add team members to the array
